43df4667dcb65e3b72a4f300c7d1481c2ed4485e
sylvartore <sylvartore@gmail.com>
2019-02-09 21:08:22 -0800

    Init Open Street Map View

--- app/src/main/java/ca/bcit/planters/treepost/MainActivity.java
+++ app/src/main/java/ca/bcit/planters/treepost/MainActivity.java
@@ -1,13 +1,39 @@
 package ca.bcit.planters.treepost;
 
-import android.support.v7.app.AppCompatActivity;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    MapView map = null;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         setContentView(R.layout.activity_main);
+        map = findViewById(R.id.map);
+
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        MapController mMapController = (MapController) map.getController();
+        mMapController.setZoom(13);
+        GeoPoint gPt = new GeoPoint(51500000, -150000);
+        mMapController.setCenter(gPt);
+    }
+
+    public void onResume(){
+        super.onResume();
+        map.onResume();
+    }
+
+    public void onPause(){
+        super.onPause();
+        map.onPause();
     }
 }
